c28dd79469e13f3b5f5ebded17c03333b598a6c4
andre4ik3 <andre4ik3@fastmail.com>
2025-06-29 06:38:19 +0300

    build with nix so I can put it on my server

--- flake.nix
+++ flake.nix
@@ -12,16 +12,41 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs = { utils, ... }@inputs: utils.lib.eachDefaultSystem (system: let
     pkgs = inputs.nixpkgs.legacyPackages.${system};
-    fenix = inputs.fenix.packages.${system};
+    toolchain = inputs.fenix.packages.${system}.stable.toolchain;
+    craneLib = (inputs.crane.mkLib pkgs).overrideToolchain toolchain;
+
+    commonArgs = {
+      src = craneLib.cleanCargoSource ./.;
+      strictDeps = true;
+    };
+
+    relay = craneLib.buildPackage (commonArgs // {
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+    });
   in {
+    checks = {
+      inherit relay;
+    };
+
+    packages = rec {
+      default = relay;
+      inherit relay;
+    };
+
+    apps = rec {
+      default = relayd;
+      relayd = relay;
+    };
+
     devShells.default = pkgs.mkShell {
       packages = [
-        (fenix.stable.toolchain)
-        pkgs.nil
+        toolchain
       ];
     };
   });
